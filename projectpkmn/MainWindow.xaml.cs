4ffd1fd6d0cee5711fb557b29aa8c25c52ed3c71
DESKTOP-KLJ2E9M\leonn <vogeldraak@gmail.com>
2016-09-05 14:09:09 +0200

    Log-In and Register system without PHPMA

--- projectpkmn/MainWindow.xaml.cs
+++ projectpkmn/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace projectpkmn
 {
@@ -20,9 +21,73 @@ namespace projectpkmn
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public string username, password;
+
         public MainWindow()
         {
             InitializeComponent();
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource =
+                new BitmapImage(new Uri("http://wallpapercave.com/wp/HolPrzE.jpg", UriKind.Absolute));
+            this.Background = myBrush;
+        }
+
+        private void tbUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        // This will be the piece of code that makes us able to login.
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var sr = new System.IO.StreamReader("C:\\" + tbRegUser.Text + "\\login.ID");
+                username = sr.ReadLine();
+                password = sr.ReadLine();
+                sr.Close();
+
+                if (username == tbUser.Text && password == tbPass.Text)
+                {
+                    
+                    pickYourOption form_pickYourOption = new pickYourOption();
+                    form_pickYourOption.Show();
+                    
+                }
+                else
+                    MessageBox.Show("This is not correct");
+
+            }
+            catch (System.IO.DirectoryNotFoundException )
+            {
+                MessageBox.Show("Alles gaat mis");
+            }
+        }
+
+        // This will be the piece of code that will make us able to register.
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter("C:\\" + tbRegUser.Text + "\\login.ID");
+                sw.Write(tbRegUser.Text + "\n" + tbRegPass.Text);               
+                sw.Close();
+            }
+            catch (System.IO.DirectoryNotFoundException ) 
+            {
+                System.IO.Directory.CreateDirectory("C:\\" + tbRegUser.Text);
+                var sw = new System.IO.StreamWriter("C:\\" + tbRegUser.Text + "\\login.ID");
+                sw.Write(tbRegUser.Text + "\n" + tbRegPass.Text);
+                sw.Close();
+            }
+        }
+
+        // This opens a new form that helps us to recover a password.
+        private void btnPassForgot_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
