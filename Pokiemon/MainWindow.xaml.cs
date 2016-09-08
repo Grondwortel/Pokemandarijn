1383401760d3edc369f56f75c242c7f7ec8f8f01
DESKTOP-KLJ2E9M\leonn <vogeldraak@gmail.com>
2016-09-08 11:54:25 +0200

    Fixed the Registration and Login

--- Pokiemon/MainWindow.xaml.cs
+++ Pokiemon/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Pokiemon
 {
@@ -20,9 +24,116 @@ namespace Pokiemon
     /// </summary>
     public partial class MainWindow : Window
     {
+        string constring = "datasource=127.0.0.1; port=3307; username=root; password=usbw;";
+
         public MainWindow()
         {
             InitializeComponent();
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource =
+                new BitmapImage(new Uri("http://www.pixelstalk.net/wp-content/uploads/2016/02/Pokemon-Backgrounds-for-desktop.jpg", UriKind.Absolute));
+            this.Background = myBrush;
+
+            
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbUser.Text.Length > 7 || tbPass.Password.Length > 8)
+            {
+                if (tbUser.Text != "Username" || tbPass.Password != "Password")
+                {
+                    //string constring = "datasource=127.0.0.1; port=3307; username=root; password=usbw;";
+                    string Query = "INSERT INTO pokemon_db.users(Username, Password, Mail)VALUES('" + tbUser.Text + "','" + tbPass.Password + "','" + tbMail.Text + "') ; ";
+                    MySqlConnection conDatabase = new MySqlConnection(constring);
+                    MySqlCommand cmdDatabase = new MySqlCommand(Query, conDatabase);
+                    MySqlDataReader myReader;
+
+                    try
+                    {
+
+                        conDatabase.Open();
+                        myReader = cmdDatabase.ExecuteReader();
+                        MessageBox.Show("Gebruiker aangemaakt!");
+                        tbPass.Clear();
+                        tbUser.Clear();
+                        tbMail.Clear();
+                        while (myReader.Read())
+                        {
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vergeet natuurlijk niet je eigen gegevens te verzinnen.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vul bij zowel gebruikersnaam als wachtwoord minimaal 8 tekens in.");
+            }
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            try
+            {
+                if (!(tbUser2.Text == string.Empty))
+                {
+                    if (!(tbPass2.Password == string.Empty))
+                    {
+                        
+                        string Query = "SELECT * FROM pokemon_db.users where Username = '" + tbUser2.Text + "'and Password = '" + tbPass2.Password + "'";
+                        MySqlConnection con = new MySqlConnection(constring);
+                        MySqlCommand cmd = new MySqlCommand(Query, con);
+                        MySqlDataReader dbr;
+                        con.Open();
+                        dbr = cmd.ExecuteReader();
+                        int count = 0;
+                        while (dbr.Read())
+                        {
+                            count = count + 1;
+                        }
+                        if (count == 1)
+                        {
+                            MessageBox.Show("username and password is correct");
+                        }
+                        
+                        else
+                        {
+                            MessageBox.Show(" username and password incorrect");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(" password empty");
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show(" username empty", "login page");
+                }
+
+                
+
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+
+            }
+
         }
     }
 }
+
+
