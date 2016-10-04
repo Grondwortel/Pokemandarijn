f6e973b6dccc4b4f610b5384bf83724407bfcc6f
DESKTOP-KLJ2E9M\leonn <vogeldraak@gmail.com>
2016-10-04 14:43:30 +0200

    PHP van het project zit er nu ook in.

---
+++ PHP Pokemon/password.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+     <title>Password and Username forgotten</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+      
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="index.html">
+                        Reset page Pokemon!
+                    </a>
+                </li>
+                <li>
+                    <a href="username.php">Username reset</a>
+                </li>
+                <li>
+                    <a href="password.php">Password reset</a>
+                </li>
+                
+                
+            </ul>
+        </div>
+     
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Password Reset</h1>
+						
+						
+						
+                        <p>Put your E-mailadres in the box and press submit</p>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+  
+
+    <script src="js/jquery.js"></script>
+
+
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- script testje -->
+	<script>
+						$("#menu-toggle").click(function(e) {
+							e.preventDefault();
+							$("#wrapper").toggleClass("toggled");
+							});
+						</script>
+
+</body>
+
+</html>
